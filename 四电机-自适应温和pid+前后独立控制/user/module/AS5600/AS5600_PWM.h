11f26576c1e96eabb83150d7a78220b050cc4134
weny09798-arch <weny09798@gmail.com>
2026-06-02 11:29:19 +0800

    1

--- 四电机-自适应温和pid+前后独立控制/user/module/AS5600/AS5600_PWM.h
+++ 四电机-自适应温和pid+前后独立控制/user/module/AS5600/AS5600_PWM.h
@@ -3,11 +3,11 @@
 #include "main.h"
 #include "adc.h"
 
-#define MAX_VALUE 4096  // 输入范围 0~4095，所以最大值为 4096  ggggggggghgggghghgghghgh88
+#define MAX_VALUE 4096  // 输入范围 0~4095，所以最大值为 4096  
 
 // 各电机1024点位定义
 #define MOTOR1_MIDPOINT 3410  // 电机1 1024点编码器值
-#define MOTOR3_MIDPOINT 330  // 电机3 1024点编码器值 原来420
+#define  MOTOR3_MIDPOINT 330  // 电机3 1024点编码器值 原来420
 
 #define MOTOR2_MIDPOINT 3500 // 电机2 1024点编码器值  
 #define MOTOR4_MIDPOINT 300  // 电机4 1024点编码器值
